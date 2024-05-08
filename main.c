85be9e017d9f1b866574ca7a49acf3d086e00c93
SaiNath Ravula <sairavula2002@gmail.com>
2024-05-08 12:18:11 +0530

    Added comments to main.c

--- main.c
+++ main.c
@@ -85,9 +85,9 @@ void vApplicationMallocFailedHook(void) {
 vApplicationMallocFailedHook() will only be called if configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.
 It is a hook function that will get called if a call to pvPortMalloc() fails.
 
-pvPortMalloc() is called internally by the kernel whenever a task, queue,timer or semaphore is created. 
- It is also called by various parts of the demo application.If heap_1.c or heap_2.c are used, then the size of the heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in FreeRTOSConfig.h, 
- and the xPortGetFreeHeapSize() API function can be used to query the size of free heap space that remains (although it does not provide information on how the remaining heap might be fragmented). 
+pvPortMalloc() is called internally by the kernel whenever a task, queue,timer or semaphore is created. It is also called by various parts of the demo application.
+If heap_1.c or heap_2.c are used, then the size of the heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used to 
+query the size of free heap space that remains (although it does not provide information on how the remaining heap might be fragmented). 
 */
 
 
@@ -102,7 +102,8 @@ vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set to 1 in
 It will be called on each iteration of the idle task. It is essential that code added to this hook function never attempts to block in any way 
 (for example, call xQueueReceive() with a block time specified, or call vTaskDelay()). 
  
-If the application makes use of the vTaskDelete() API function (as this demo application does) then it is also important that vApplicationIdleHook() is permitted to return to its calling function, 
+If the application makes use of the vTaskDelete() API function (as this demo application does) then it is also important that 
+vApplicationIdleHook() is permitted to return to its calling function, 
 because it is the responsibility of the idle task to clean up memory allocated by the kernel to any task that has since been deleted. 
 */
  
@@ -164,7 +165,8 @@ void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackT
 }
 
 /* 
-configUSE_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the application must provide an implementation of vApplicationGetTimerTaskMemory() to provide the memory that is used by the Timer service task. 
+configUSE_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the application must provide an implementation of 
+vApplicationGetTimerTaskMemory() to provide the memory that is used by the Timer service task. 
 */
 
 //=========================================================================================================================================
